21ada86b19a490aec0dc5f72bbeacfba69ef58e0
Krzysztof <krzysztof211@gmail.com>
2017-11-01 14:31:26 +0100

    structure created

--- PTS/PTS/PTS.cpp
+++ PTS/PTS/PTS.cpp
@@ -7,20 +7,31 @@
 #include <vector>
 using namespace std;
 
+struct Task {
+	int id;//job number
+	int submit;//r_j - submit time
+	int run;//p_j - run time
+	int proc;//size_j - processors needed 
+};
+
 class Data {
 private:
-	vector<int> data_tab[4];
-	//tab[0] - job number
-	//tab[1] - r_j - submit time
-	//tab[2] - r_j - run time
-	//tab[3] - size_j - processors needed 
+	vector<Task> data;
+	int maxNodes;
+	int maxProcs;
+	
 
 public:
 	void getData(string path)
 	{
 		ifstream plik;
+		string line;
 		plik.open(path, ios::in);
+		while (!plik.eof())
+		{
+			getline(plik, line);
 
+		}
 		plik.close();
 	}
 
