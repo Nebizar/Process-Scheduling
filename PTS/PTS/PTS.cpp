ca1c0eb24cb192e1b88539d2bd45ab2f95444b11
Krzysztof <krzysztof211@gmail.com>
2017-11-01 21:09:32 +0100

    Getting data

--- PTS/PTS/PTS.cpp
+++ PTS/PTS/PTS.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 struct Task {
@@ -19,26 +20,65 @@ private:
 	vector<Task> data;
 	int maxNodes;
 	int maxProcs;
+	int counter = 0;
 	
 
 public:
 	void getData(string path)
 	{
+		int n;
+		
+		vector <int> pom;
 		ifstream plik;
 		string line;
 		plik.open(path, ios::in);
 		while (!plik.eof())
 		{
 			getline(plik, line);
+			if (line[0] == ';')
+			{
+				string tmp;
+				int value;
+				char c;
+				stringstream ss(line);
+				ss >> c >> tmp >> value;
+				if (tmp == "MaxNodes:") maxNodes = value;
+				else if (tmp == "MaxProcs:") maxProcs = value;
+			}
+			else
+			{
+				Task task;
+				stringstream stream(line);
+				while (stream >> n)
+				{
+					pom.push_back(n);
+				}
+				task.id = pom[0];
+				task.submit = pom[1];
+				task.run = pom[3];
+				task.proc = pom[4];
+				pom.clear();
+				data.push_back(task);
+				counter++;
+			}
 
 		}
 		plik.close();
+	}
+	void showData()
+	{
+		cout << maxNodes << endl;
+		cout << maxProcs << endl;
+		for (int i = 0; i < counter; i++)
+			cout << data[i].id << "\t" << data[i].submit << "\t" << data[i].run << "\t" << data[i].proc << endl;
 	}
 
 };
 
 int main()
 {
-
+	Data tasklist;
+	tasklist.getData("data1.txt");
+	tasklist.showData();
 	return EXIT_SUCCESS;
 }
